52ba88355fefd1246ce2b6bc81309a6f58e8a5bc
riborges <ricardodias_5@outlook.es>
2021-08-28 13:02:28 +0200

    Day 9, leetcode

--- leetcode/JS/mergeTwoSortedLists.js
+++ leetcode/JS/mergeTwoSortedLists.js
@@ -41,3 +41,48 @@
     
     
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(l1, l2) {
+    if(l1 == null && l2 == null) return l1
+    if(l1 == null && l2 != null) return l2
+    if(l1 != null && l2 == null) return l1
+    
+    let result = new ListNode(-1, null)
+    
+    let res = result
+    
+    while(l1 != null && l2 != null){
+        if(l1.val <= l2.val){
+            res.next = new ListNode(l1.val, null)
+            l1 = l1.next
+        }else{
+            res.next = new ListNode(l2.val, null)
+            l2 = l2.next
+        }
+        
+        res = res.next
+    }
+    
+    if(l1 != null){
+        res.next = l1
+    }
+    
+    if(l2 != null){
+        res.next = l2
+    }
+    
+    return result.next
+};
--- leetcode/JS/reverseLinkedList.js
+++ leetcode/JS/reverseLinkedList.js
@@ -43,3 +43,30 @@
     return result
     
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    if(head == null) return null
+    
+    let current = head
+    
+    while(current.next != null){
+        let tmp = current.next
+        current.next = current.next.next
+        tmp.next = head
+        head = tmp
+    }
+    
+    return head
+};

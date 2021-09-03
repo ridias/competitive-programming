039cc6ecaa8dfdc494934c42aeb2de2270410248
riborges <ricardodias_5@outlook.es>
2021-09-03 10:16:15 +0200

    Day 15, leetcode

--- leetcode/JS/bestTimeToBuyAndSellStock.js
+++ leetcode/JS/bestTimeToBuyAndSellStock.js
@@ -1,5 +1,8 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+
+// O(n^2)
+
 /**
  * @param {number[]} prices
  * @return {number}
@@ -18,4 +21,34 @@
     }
     
     return profit
+};
+
+
+//O(n)
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let min = prices[0]
+    let max = prices[0]
+    
+    let total = 0;
+    
+    
+    for(let i = 1; i < prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i]
+            max = prices[i]
+        }
+        
+        if(prices[i] > max){
+            max = prices[i]
+        }
+        
+        if(max - min > total) total = max - min
+    }
+    
+    return total
 };
